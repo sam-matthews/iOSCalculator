e1fde8a8d78a0998fa455dfa242f73e24b2602fc
Sam <sam@sams-macbook-pro.local>
2017-07-02 21:37:32 +1200

    First commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var firstNumberText = ""
+    var secondNumberText = ""
+    var op = ""
+    var isFirstNumber = true
+    var hasOp = false
+    var canClear = true
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +29,65 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func calculate() -> Double {
+        let firstNumber = Double(firstNumberText)!
+        let secondNumber = Double(secondNumberText)!
+        firstNumberText = ""
+        secondNumberText = ""
+        
+        if (op == "/" && firstNumber == 0 || secondNumber == 0) {
+            return 9999999999
+        }
+        
+        switch op {
+        case "+": return firstNumber + secondNumber
+        case "-": return firstNumber - secondNumber
+        case "*": return firstNumber * secondNumber
+        case "/": return firstNumber / secondNumber
+        default: return 0
+        }
+    }
+    
+    // This func is fired whenever one of the number buttons is pressed.
+    // Let commands create a variable in swift - so they dont need to be defined elsewhere.
+    
+    @IBAction func handleButtonPress(_ sender: UIButton) {
+        if canClear {
+            resultLabel.text = ""
+            canClear = false
+        }
+        let currentText = resultLabel.text!
+        let textLabel = sender.titleLabel?.text
+        
+        if let text = textLabel {
+            switch text {
+            case "+", "*", "/", "-":
+                if hasOp {
+                    return
+                }
+                op = text
+                isFirstNumber = false
+                hasOp = true
+                resultLabel.text = "\(currentText) \(op) "
+                break
+            case "=":
+                isFirstNumber = true
+                hasOp = false
+                canClear = true
+                let result = calculate()
+                resultLabel.text = "\(result)"
+                break
+            default:
+                if isFirstNumber {
+                    firstNumberText = "\(firstNumberText)\(text)"
+                } else {
+                    secondNumberText = "\(secondNumberText)\(text)"
+                }
+                resultLabel.text = "\(currentText)\(text)"
+                break;
+            }
+        }
+    }
+    
 }
 
